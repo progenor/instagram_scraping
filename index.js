d5459df786f00533f6a605519070b3a3a7cbd581
progenor <nero6126@gmail.com>
2023-04-22 23:57:32 +0300

    added working feature

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const puppeteer = require("puppeteer");
 
+const iDontCareAboutCookies =
+  "C:\\Users\\ner61\\AppData\\Local\\Vivaldi\\User Data\\Default\\Extensions\\fihnjjcciajhdojfnbdddfaoknhalnja\\3.4.6_0";
+
 async function run() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: [
+      `--disable-extensions-except=${iDontCareAboutCookies}`,
+      `--load-extension=${iDontCareAboutCookies}`,
+      "--enable-automation",
+    ],
+  });
   const page = await browser.newPage();
-  await page.goto("https://www.google.com");
-  await page.screenshot({ path: "google.png" });
+  await page.goto("https://www.instagram.com/mortyexplainsit/");
+  await page.waitForSelector("main");
+  await delay(3000);
+  await page.pdf({ path: "page.pdf", format: "A4" });
   await browser.close();
+}
+
+function delay(time) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time);
+  });
 }
 
 run();
